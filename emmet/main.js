f6a615f4bd41afcab4e098c784eb241a09462bec
Arjan <vanderveenarjan@gmail.com>
2021-07-04 10:57:14 +0200

    Update
    Working on the coding syntax and how it looks.

--- emmet/main.js
+++ emmet/main.js
@@ -1,11 +1,17 @@
-// https://entitycode.com/
+const trace = (msg) => {
+  return console.log(msg);
+}
 
+// https://entitycode.com/
+// class name code?
+// code -> turn into pre tag with the tags in spans for colors
 let p = document.getElementsByTagName("pre");
-let a = "23asaS"
-let b = "&qwe4;"
 // should return a match if starts with '&', 
 // ends on ';' and has 0 or more characters in between
 let isCodeIdentity = /^&[\w\W]*;$/gm;
+let groupCI = /^&[\w]+;([\w\W]+)&[\w]+;$/
+let d =  /div/gm;
+let e =   `<span class="code-tag">$&</span>`
 
-console.log(isCodeIdentity.test(b));
-console.log(isCodeIdentity.test(a));
+trace(p[0].innerHTML.replace(d,e));
+ p[0].innerHTML = p[0].innerHTML.replace(d,e);
