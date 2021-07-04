d1d999124041ec63be2e5ae59377913db8ed0f9f
Arjan <69347644+skarjan@users.noreply.github.com>
2021-07-04 15:25:11 +0200

    Update
    Harstuck on the syntax highlighter, going to start again on next commit

--- emmet/main.js
+++ emmet/main.js
@@ -9,8 +9,20 @@ let pre = document.getElementsByTagName("pre");
 let groupCI = /^&[\w]+;([\w\W]+)&[\w]+;$/
 
 
-let div =  /div/gm;
-let htmlTagSpan =   `<span class="code--html">$&</span>`
+let divEl =  /div/gm;
+let htmlTagSpan =   `<span class="code--html">$&</span>`;
 
-trace(pre[0].innerHTML.replace(div,htmlTagSpan));
- pre[0].innerHTML = pre[0].innerHTML.replace(div,htmlTagSpan);
+let classSelect = /(class)(=)("[\w]*")/g
+let classSelectSpan=   `<span class="code--html--class">$1</span>$2<span class="code--html--class--content">$3</span>`;
+
+
+
+
+// style classes first, 
+// because this function itself adds classes which would also be styled
+// pre[0].innerHTML = pre[0].innerHTML.replace(classSelect,classSelectSpan);
+
+// pre[0].innerHTML = pre[0].innerHTML.replace(divEl,htmlTagSpan);
+pre[0].innerHTML = pre[0].innerHTML.replace(classSelect, classSelectSpan);
+trace(pre[0].innerHTML);
+// trace(pre[0].innerHTML.replace(divEl,htmlTagSpan));
